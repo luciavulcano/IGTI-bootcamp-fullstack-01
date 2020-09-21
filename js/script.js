34426f5c3b34c62d4bc64fc88517c840545ebcd4
Lúcia Vulcano <vulcanolucia@gmail.com>
2020-09-21 16:55:02 -0300

    primeira versao

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let red = document.querySelector('#red');
+let green = document.querySelector('#green');
+let blue = document.querySelector('#blue');
+let rValue = document.querySelector('#redNumber');
+let gValue = document.querySelector('#greenNumber');
+let bValue = document.querySelector('#blueNumber');
+
+window.onload = () => {
+  red.addEventListener('change', changeRed);
+  green.addEventListener('change', changeGreen);
+  blue.addEventListener('change', changeBlue);
+}
+
+function changeRed(event) {
+  rValue.value = event.target.value;
+  changeColor();
+}
+
+function changeGreen(event) {
+  gValue.value = event.target.value;
+  changeColor();
+}
+
+function changeBlue(event) {
+  bValue.value = event.target.value;
+  changeColor();
+}
+
+function changeColor() {
+  let r = rValue.value;
+  let g = gValue.value;
+  let b = bValue.value;
+  let divColor = document.querySelector('#color');
+  divColor.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
